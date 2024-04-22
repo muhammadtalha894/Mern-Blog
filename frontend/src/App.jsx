dde5630f916cfeeda120f078e053c73cb61fc71b
Muhammad Talha <talhatam178@gmail.com>
2024-04-22 15:35:44 +0500

    Create Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import { Home } from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 function App() {
-  return <div className='text-purple-950 text-3xl'> hello</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/sign-in' element={<SignIn />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
+          <Route path='/dashboard' element={<Dashboard />}></Route>
+          <Route path='/projects' element={<Projects />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
